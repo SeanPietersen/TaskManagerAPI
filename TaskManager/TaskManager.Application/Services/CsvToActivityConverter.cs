97df01a68734c4561cd274fce21ee5bbc7fb442b
Sean Pietersen <seanpietersen7@gmail.com>
2022-08-05 15:49:07 +0200

    Feat: Added convert csv to actvitiy with test

---
+++ TaskManager/TaskManager.Application/Services/CsvToActivityConverter.cs
@@ -0,0 +1,36 @@
+﻿using TaskManager.Domain;
+
+namespace TaskManager.Application.Services
+{
+    public class CsvToActivityConverter : ICsvToActivityConverter
+    {
+        private readonly IDueDateCalculater _dueDateCalculater;
+        private readonly ICsvStringDateToDateConverter _csvStringDateToDateConverter;
+
+        public CsvToActivityConverter(IDueDateCalculater dueDateCalculater, 
+                                      ICsvStringDateToDateConverter csvStringDateToDateConverter)
+        {
+            _dueDateCalculater = dueDateCalculater;
+            _csvStringDateToDateConverter = csvStringDateToDateConverter;
+        }
+        public Activity ConvertCsvToActivity(String line)
+        {
+            string[] columns = line.Split(',');
+            var activityToReturn = new Activity()
+            {
+                Id = int.Parse(columns[0].ToString()),
+                Description = columns[1].Replace(@"'", ""),
+                Client = columns[2].Replace(@"'", ""),
+                StartDate = _csvStringDateToDateConverter.ConvertCsvStringDateToDate(columns[3].Replace(@"'", "")),
+                Dutration = int.Parse(columns[4].ToString()),
+                Task1 = columns[5].Replace(@"'", ""),
+                Task2 = columns[6].Replace(@"'", ""),
+                Task3 = columns[7].Replace(@"'", ""),
+                Task4 = columns[8].Replace(@"'", ""),
+                Task5 = columns[9].Replace(@"'", ""),
+                DueDate = _dueDateCalculater.CalculateDueDate(_csvStringDateToDateConverter.ConvertCsvStringDateToDate(columns[3].Replace(@"'", "")), int.Parse(columns[4].ToString()))
+            };
+            return activityToReturn;
+        }
+    }
+}
---
+++ TaskManager/TaskManager.Application/Services/ICsvToActivityConverter.cs
@@ -0,0 +1,9 @@
+﻿using TaskManager.Domain;
+
+namespace TaskManager.Application.Services
+{
+    public interface ICsvToActivityConverter
+    {
+        Activity ConvertCsvToActivity(String line);
+    }
+}
