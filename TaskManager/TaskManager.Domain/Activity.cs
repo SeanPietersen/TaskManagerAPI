a197a428d5f49ccf12b9419a6f05a43fcf1e59ab
Sean Pietersen <seanpietersen7@gmail.com>
2022-08-05 14:40:59 +0200

    Feat: Added Activity class

---
+++ TaskManager/TaskManager.Domain/Activity.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManager.Domain
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Dutration { get; set; }
+        public string Task1 { get; set; }
+        public string Task2 { get; set; }
+        public string Task3 { get; set; }
+        public string Task4 { get; set; }
+        public string Task5 { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
