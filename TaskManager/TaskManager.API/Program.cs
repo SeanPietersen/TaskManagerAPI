b2f574744d8f16617ed49fb8ea5cec3e058a89bc
Sean Pietersen <seanpietersen7@gmail.com>
2022-08-06 15:59:47 +0200

    Feat: Added Cors to program.cs to allow cross domain requests

--- TaskManager/TaskManager.API/Program.cs
+++ TaskManager/TaskManager.API/Program.cs
@@ -33,6 +33,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+   .AllowAnyOrigin()
+   .AllowAnyMethod()
+   .AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
